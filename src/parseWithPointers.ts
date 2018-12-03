7d7083c555e351383e6f1f0861a9dcc4dac4a2ac
Jakub Rożek <jakub@stoplight.io>
2018-12-03 18:58:16 +0100

    feat: remove parseJsonWithPointers

--- src/parseWithPointers.ts
+++ src/parseWithPointers.ts
@@ -1,13 +1,13 @@
 import { Processor } from 'unified';
-import { safeStringify } from '@stoplight/json/safeStringify';
-import { parseWithPointers as parseJsonWithPointers } from '@stoplight/json/parseWithPointers';
 import { IParserResult } from '@stoplight/types';
 import { ParseOpts } from 'remark-parse';
 
 import { parse } from './parse';
 
 export const parseWithPointers = <T>(value: string, opts?: ParseOpts, processor?: Processor): IParserResult<T> => {
-  return parseJsonWithPointers(safeStringify(parse(value, opts, processor)));
+  return {
+    data: parse(value, opts, processor),
+    pointers: {}, // todo: implement actual functionality
+    validations: [],
+  };
 };
-
-/** END parseWithPointers.ts */
