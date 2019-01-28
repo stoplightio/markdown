9454184121c1215185f12c8b11c1457386e60bfa
Chris <chrisnlott@gmail.com>
2019-01-28 17:53:25 -0600

    fix: processor needs to be called as a function

--- src/__tests__/stringify.spec.ts
+++ src/__tests__/stringify.spec.ts
@@ -8,4 +8,12 @@ describe('stringify', () => {
       stringify(JSON.parse(fs.readFileSync(path.resolve(__dirname, './fixtures/simple.json'), 'utf-8')))
     ).toMatchSnapshot();
   });
+
+  it('should work when called multiple times in a row', () => {
+    // This tests to make sure the processor isn't frozen: https://github.com/unifiedjs/unified/blob/7ee2c8f563f0ebe330cd76496be9ba405a1cd023/readme.md#processorfreeze
+
+    const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, './fixtures/simple.json'), 'utf-8'));
+
+    expect(stringify(json)).toBe(stringify(json));
+  });
 });
