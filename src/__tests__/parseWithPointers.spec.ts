57cb3b79e018f1e8e9fd48396b1b90687cfcd925
Jakub Rożek <jakub@stoplight.io>
2019-03-21 12:50:09 +0100

    test: reduce size of snapshot

--- src/__tests__/parseWithPointers.spec.ts
+++ src/__tests__/parseWithPointers.spec.ts
@@ -2,6 +2,8 @@ import { parseWithPointers } from '../parseWithPointers';
 
 describe('parseWithPointers', () => {
   it('should parse simple', () => {
-    expect(parseWithPointers('**simple**')).toMatchSnapshot();
+    expect(parseWithPointers('**simple**')).toMatchSnapshot({
+      ast: expect.any(Object),
+    });
   });
 });
