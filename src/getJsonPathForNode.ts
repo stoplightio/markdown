4bdc274ab7a78e9d795044f9f51d8ebce026e15e
Jakub Rożek <jakub@stoplight.io>
2019-07-26 22:28:58 +0700

    refactor: remove "!" clobbers

--- src/getJsonPathForNode.ts
+++ src/getJsonPathForNode.ts
@@ -8,16 +8,18 @@ export const getJsonPathForNode = (root: Unist.Parent, node: Unist.Node): JsonPa
 };
 
 function findNode(root: Unist.Parent | Unist.Node, node: Unist.Node, path: JsonPath): Unist.Node | undefined {
+  if (node.position === undefined || root.position === undefined) return;
+
   if (
-    node.position!.start.line === root.position!.start.line &&
-    node.position!.end.line === root.position!.end.line &&
-    node.position!.start.column === root.position!.start.column &&
-    node.position!.end.column === root.position!.end.column
+    node.position.start.line === root.position.start.line &&
+    node.position.end.line === root.position.end.line &&
+    node.position.start.column === root.position.start.column &&
+    node.position.end.column === root.position.end.column
   ) {
     return node;
   }
 
-  if (node.position!.start.line >= root.position!.start.line && node.position!.end.line <= root.position!.end.line) {
+  if (node.position.start.line >= root.position.start.line && node.position.end.line <= root.position.end.line) {
     const { children } = root;
     if (Array.isArray(children)) {
       for (let i = 0; i < children.length; i++) {
