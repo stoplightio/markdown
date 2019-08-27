37e41cde061e56554ca753e8215a70bc8c6a7485
Jakub Rożek <jakub@stoplight.io>
2019-08-27 16:32:32 +0200

    feat: markdown builder

---
+++ src/__tests__/builder.spec.ts
@@ -0,0 +1,32 @@
+import { Builder } from '../builder';
+
+describe('Builder', () => {
+  describe('#addMarkdown', () => {
+    it('takes any markdown, parses it and attaches to the root node', () => {
+      const b = new Builder();
+      const markdown = '**foo**\n\nwooo';
+      b.addMarkdown(markdown);
+      expect(b.toString()).toEqual(`${markdown}\n`);
+    });
+  });
+
+  describe('#addChild', () => {
+    it('takes any Mdast child and places it under the root node', () => {
+      const b = new Builder();
+
+      const node = {
+        type: 'heading',
+        depth: 1,
+        children: [
+          {
+            type: 'text',
+            value: 'Foo',
+          },
+        ],
+      };
+
+      b.addChild(node);
+      expect(b.toString()).toEqual('# Foo\n');
+    });
+  });
+});
---
+++ src/builder.ts
@@ -0,0 +1,27 @@
+import * as Unist from 'unist';
+import { Reader } from './reader';
+import { ILangReader } from './reader/types';
+import { stringify } from './stringify';
+
+export class Builder {
+  public root: Unist.Parent;
+
+  constructor(public reader: ILangReader = new Reader()) {
+    this.root = {
+      type: 'root',
+      children: [],
+    };
+  }
+
+  public addMarkdown(markdown: string) {
+    this.root.children.push(...this.reader.fromLang(markdown).children);
+  }
+
+  public addChild(node: Unist.Node) {
+    this.root.children.push(node);
+  }
+
+  public toString() {
+    return stringify(this.root);
+  }
+}
