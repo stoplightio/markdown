5b23408af38da8d7b3559c7bc040583ffbf4115b
Jakub Rożek <jakub@stoplight.io>
2020-03-06 00:35:38 +0800

    feat(parser): recognize Jira blocks (#24)
    * feat(parser): recognize Jira blocks
    
    * feat: stringify

--- src/parse.ts
+++ src/parse.ts
@@ -2,6 +2,7 @@ import remarkParse, { RemarkParseOptions } from 'remark-parse';
 import unified from 'unified';
 import * as Unist from 'unist';
 const frontmatter = require('remark-frontmatter');
+import jiraBlocks from './plugins/jiraBlocks';
 
 const defaultOpts: Partial<RemarkParseOptions> = {
   commonmark: true,
@@ -10,6 +11,7 @@ const defaultOpts: Partial<RemarkParseOptions> = {
 
 const defaultProcessor = unified()
   .use<RemarkParseOptions[]>(remarkParse)
+  .use(jiraBlocks)
   .use(frontmatter, ['yaml']);
 
 export const parse = (
